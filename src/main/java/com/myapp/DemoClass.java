75c21b1cabff598b1395008d7cab8800c15f2453
vikas kumar gupta <mrvikaskumargupta@gmail.com>
2022-06-17 15:58:14 +0530

    Added project code to local repository

---
+++ src/main/java/com/myapp/DemoClass.java
@@ -0,0 +1,12 @@
+package com.myapp;
+
+public class DemoClass {
+	
+	public static void hello() {
+		System.out.println("Say hello to vikas");
+	}
+	public static void main(String[] args) {
+		hello();
+		System.out.println("Hello vikas! How are you");
+	}
+}
