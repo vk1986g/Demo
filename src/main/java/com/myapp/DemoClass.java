eb2cb4411b4379ff8fec712bc9dea6ddf2246d4b
vikas kumar gupta <mrvikaskumargupta@gmail.com>
2022-06-22 16:34:20 +0530

    Added a message

--- src/main/java/com/myapp/DemoClass.java
+++ src/main/java/com/myapp/DemoClass.java
@@ -3,6 +3,7 @@ package com.myapp;
 public class DemoClass {
 	
 	public static void hello() {
+		System.out.println("hi...................");
 		System.out.println("Say hello to vikas");
 	}
 	public static void main(String[] args) {
